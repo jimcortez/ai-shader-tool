c69ae383d1cf7d2bcc5158f5c8af4acd728e3888
Jim Cortez <jim@jimcortez.com>
2025-07-10 22:52:36 -0700

    Fix leftover merge conflict marker in __init__.py

--- src/isf_shader_renderer/__init__.py
+++ src/isf_shader_renderer/__init__.py
@@ -4,15 +4,7 @@ __version__ = "0.1.0"
 __author__ = "Jim Cortez"
 __email__ = "jim.cortez@gmail.com"
 
-<<<<<<< HEAD
-from .config import Config, load_config
-from .renderer import ShaderRenderer
-
-__all__ = ["Config", "load_config", "ShaderRenderer"] 
-=======
-from .cli import main
 from .config import ShaderRendererConfig, load_config
 from .renderer import ShaderRenderer
 
-__all__ = ["main", "ShaderRendererConfig", "load_config", "ShaderRenderer"]
->>>>>>> 43e6b06c995869d9d0ab4c0203d0367da54c3d54
+__all__ = ["ShaderRendererConfig", "load_config", "ShaderRenderer"]
